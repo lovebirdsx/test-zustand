b62a5c15d24441f0299f088c7203fb3bfdf1b543
lovebird <lovebirdsx@gmail.com>
2025-03-14 01:03:35 +0800

    加入zustand测试

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { create } from 'zustand'
+
+const useBearStore = create((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}))
+
+function BearCounter() {
+  const bears = useBearStore((state) => state.bears)
+  const increasePopulation = useBearStore((state) => state.increasePopulation)
+  const removeAllBears = useBearStore((state) => state.removeAllBears)
+
+  return (
+    <div>
+      <h1>{bears} around here ...</h1>
+      <button onClick={increasePopulation}>one up</button>
+      <button onClick={removeAllBears}>remove all</button>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div>
       <h1>React App</h1>
-      <button onClick={() => alert('Hello World')}>Click Me</button>
+      <button onClick={() => alert('Hello World')}>Click Me !</button>
+      <BearCounter />
     </div>
   );
 }
